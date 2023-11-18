0d137c3d0845f1689578aa7682254cdec4fdc69f
vanshitaprep <81114486+vanshitaroop@users.noreply.github.com>
2023-11-18 18:11:06 +0530

    redux and theme set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-import { BrowserRouter,Navigate,Routes,Route } from 'react-router-dom';
-import Homepage from './scenes/homePage';
-import LoginPage from './scenes/loginPage';
-import ProfilePage from './scenes/profilePage';
+// import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Homepage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useMemo } from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 function App() {
+  const mode = useSelector((state) => state.mode);
+  // Use useMemo when you want to memoize a value and recalculate it only when certain dependencies change.
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
       <BrowserRouter>
+       <ThemeProvider theme={theme}>
+        <CssBaseline/>
         <Routes>
-          <Route path="/" element={<LoginPage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/profile/:userId" element={<LoginPage/>}/> 
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
+       </ThemeProvider>
       </BrowserRouter>
     </div>
   );
